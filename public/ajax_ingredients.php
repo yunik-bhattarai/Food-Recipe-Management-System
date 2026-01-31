f5b97e934a3be6d22108f61deff78c3f2ad7a5fc
Yunik Bhattarai <yunik.b22@gmail.com>
2026-01-31 16:52:15 +0545

    Move application into public_html for college server

--- public/ajax_ingredients.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "../config/db.php";
-$term = $_GET['term'] ?? '';
-$stmt = $pdo->prepare("SELECT DISTINCT ingredient_name FROM ingredients WHERE ingredient_name LIKE ? LIMIT 5");
-$stmt->execute(["%$term%"]);
-echo json_encode($stmt->fetchAll(PDO::FETCH_COLUMN));
